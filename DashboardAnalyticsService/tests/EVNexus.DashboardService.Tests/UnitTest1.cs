01044a2c5570f0a00c586a4c97b43b098fbd045c
hiruni <it24610786@my.sliit.lk>
2026-08-20 23:50:41 +0530

    feat: initialize EVNexus microservices and test projects

---
+++ DashboardAnalyticsService/tests/EVNexus.DashboardService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EVNexus.DashboardService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ MapService/tests/EVNexus.MapService.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EVNexus.MapService.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
